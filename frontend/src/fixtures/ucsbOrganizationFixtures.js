fadfc5b571640664c4daac3dddd6cc7332af6863
Benjamin Soo <benjaminsoo@ucsb.edu>
2025-04-30 19:21:27 -0700

    fixed formatting issues

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,31 +1,30 @@
 const ucsbOrganizationFixtures = {
-    oneOrganization: {
-        "orgCode": "APA",
-        "orgTranslationShort": "Alpha Phi Aloha",
-        "orgTranslation": "Alpha Phi Alpha",
-        "inactive": true
-      },
-    threeDates: [
-        {
-            "orgCode": "SKY",
-            "orgTranslationShort": "skydive",
-            "orgTranslation": "skydiving",
-            "inactive": true
-          },
-          {
-            "orgCode": "APA",
-            "orgTranslationShort": "Alpha Phi Aloha",
-            "orgTranslation": "Alpha Phi Alpha",
-            "inactive": true
-          },
-          {
-            "orgCode": "ZPR",
-            "orgTranslationShort": "Zeta Phi Rho",
-            "orgTranslation": "Zeta Phi Rho",
-            "inactive": false
-          }
-    ]
-  };
-  
-  export { ucsbOrganizationFixtures };
-  
+  oneOrganization: {
+    orgCode: "APA",
+    orgTranslationShort: "Alpha Phi Aloha",
+    orgTranslation: "Alpha Phi Alpha",
+    inactive: true,
+  },
+  threeDates: [
+    {
+      orgCode: "SKY",
+      orgTranslationShort: "skydive",
+      orgTranslation: "skydiving",
+      inactive: true,
+    },
+    {
+      orgCode: "APA",
+      orgTranslationShort: "Alpha Phi Aloha",
+      orgTranslation: "Alpha Phi Alpha",
+      inactive: true,
+    },
+    {
+      orgCode: "ZPR",
+      orgTranslationShort: "Zeta Phi Rho",
+      orgTranslation: "Zeta Phi Rho",
+      inactive: false,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
