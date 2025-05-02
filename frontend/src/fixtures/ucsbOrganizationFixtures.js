36eb5a75f52b169f817ac0b46a6c5c9050d4d97b
Benjamin Soo <benjaminsoo@ucsb.edu>
2025-05-01 17:49:23 -0700

    adding files

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,31 @@
+const ucsbOrganizationFixtures = {
+    oneOrganization: {
+      orgCode: "APA",
+      orgTranslationShort: "Alpha Phi Aloha",
+      orgTranslation: "Alpha Phi Alpha",
+      inactive: true,
+    },
+    threeDates: [
+      {
+        orgCode: "SKY",
+        orgTranslationShort: "skydive",
+        orgTranslation: "skydiving",
+        inactive: true,
+      },
+      {
+        orgCode: "APA",
+        orgTranslationShort: "Alpha Phi Aloha",
+        orgTranslation: "Alpha Phi Alpha",
+        inactive: true,
+      },
+      {
+        orgCode: "ZPR",
+        orgTranslationShort: "Zeta Phi Rho",
+        orgTranslation: "Zeta Phi Rho",
+        inactive: false,
+      },
+    ],
+  };
+  
+  export { ucsbOrganizationFixtures };
+  
