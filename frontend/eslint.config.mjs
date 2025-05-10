a5d1a2e0e527c29e5acf4d585a7bec2508b8d6a8
mihir-r-kondapalli <mihir.kondapalli@gmail.com>
2025-05-09 19:28:39 -0700

    fixed eslint fr

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
 ]);
